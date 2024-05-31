dc8c9abfd8d413ba3dddf0b37e3792e839de2656
solomonrazak <solomonrazak99@gmail.com>
2024-05-31 05:43:23 -0700

    nextUI and overview image removed

--- src/components/admin/Overview.jsx
+++ src/components/admin/Overview.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import overview from '../../assets/images/overview.PNG';
+
 
 const Overview = () => {
   return (
     <div className="w-full">
-        <img src={overview} width={900} height={500} className="object-cover"/>
+       
     </div>
   )
 }
