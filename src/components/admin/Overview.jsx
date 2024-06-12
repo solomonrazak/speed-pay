0a51d949e999c39727b854d2b1aca621b0192abd
solomonrazak <solomonrazak99@gmail.com>
2024-06-12 03:43:34 -0700

    Update Overview.jsx

--- src/components/admin/Overview.jsx
+++ src/components/admin/Overview.jsx
@@ -26,8 +26,8 @@ const Overview = () => {
         <div className="flex gap-3 bg-white w-[210px] py-4 pl-3 pr-1 justify-center rounded-md">
             <img src={trya} alt="" width={50} height={60} className="object-contain"/>
             <div className="leading-3 space-y-1">
-              <h1 className="font-bold text-[20px]">75</h1>
-              <p className="text-[10px] flex flex-col"><span>Total Generated</span><span>Invoices</span></p>
+              <h1 className="font-bold text-[20px]">357</h1>
+              <p className="text-[10px] flex flex-col">Total Transactions</p>
               <div className="flex items-center gap-1">
                 <div className='bg-green-100 rounded-full'><img src={arrowgreen} className="object-contain p-1 rounded-full"/></div>
                 <p className="text-[10px] text-gray-500">4% (30days)</p>
@@ -37,22 +37,22 @@ const Overview = () => {
         <div className="flex gap-3 bg-white w-[210px] py-4 pl-3 pr-1 justify-center rounded-md">
             <img src={trya} alt="" width={50} height={60} className="object-contain"/>
             <div className="leading-3 space-y-1">
-              <h1 className="font-bold text-[20px]">75</h1>
-              <p className="text-[10px] flex flex-col"><span>Total Generated</span><span>Invoices</span></p>
+              <h1 className="font-bold text-[20px]">65</h1>
+              <p className="text-[10px] flex flex-col">Total Canceled</p>
               <div className="flex items-center gap-1">
                 <div className='bg-green-100 rounded-full'><img src={arrowgreen} className="object-contain p-1 rounded-full"/></div>
-                <p className="text-[10px] text-gray-500">4% (30days)</p>
+                <p className="text-[10px] text-gray-500">25% (30days)</p>
               </div>
             </div>
         </div>
         <div className="flex gap-3 bg-white w-[210px] py-4 pl-3 pr-1 justify-center rounded-md">
             <img src={trya} alt="" width={50} height={60} className="object-contain"/>
             <div className="leading-3 space-y-1">
-              <h1 className="font-bold text-[20px]">75</h1>
-              <p className="text-[10px] flex flex-col"><span>Total Generated</span><span>Invoices</span></p>
+              <h1 className="font-bold text-[20px]">$128</h1>
+              <p className="text-[10px] flex flex-col">Total Revenue</p>
               <div className="flex items-center gap-1">
                 <div className='bg-green-100 rounded-full'><img src={arrowgreen} className="object-contain p-1 rounded-full"/></div>
-                <p className="text-[10px] text-gray-500">4% (30days)</p>
+                <p className="text-[10px] text-gray-500">12% (30days)</p>
               </div>
             </div>
         </div>
